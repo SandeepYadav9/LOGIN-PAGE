ead679883efcc55f2dd07662b626ef6de0d5157f
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-01 15:43:44 +0530

    LOGIN

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const emailIsVlid = email.trim() !== "";
+  const invalidEmail = !emailIsVlid && error;
+
+  const isValidPassword = email.trim() !== "";
+  const invalidPassword = !isValidPassword && error;
+
+  const submitHandler = (e) => {
+    e.priventDefault();
+    setError(true);
+    if (!emailIsVlid && !invalidPassword) {
+      return;
+    }
+    setEmail("");
+    setPassword("");
+    setError(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="main">
+      <div className="container">
+      <form>
+        <h3>LoginPage</h3>
+
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          onBlur={() => {
+            setError(true);
+          }}
+          value={email}
+          placeholder="Email"
+        />
+
+        <br />
+
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          onBlur={() => {
+            setError(true);
+          }}
+          value={password}
+          placeholder="Password"
+        />
+        {/* {invalidPassword && <p>Somthing Went Worng</p>} */}
+        {invalidEmail && <p>Somthing Went Worng</p>}
+        <div>
+          <button onSubmit={submitHandler}>Login</button>
+        </div>
+      </form>
+
+      <div className="imageContent">
+        <img src="" alt="" className="image" />
+        <p>Lorem ipsum dolor sit, amet</p>
+      </div>
+      </div>
     </div>
   );
 }
